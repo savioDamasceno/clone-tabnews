ca4464869fec44aeefdab9334179a44823df5e0d
Sávio Oliveira Damasceno <73443788+savioDamasceno@users.noreply.github.com>
2025-03-08 01:56:21 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows);
+  const dataBaseVersionResult = await database.query("SHOW SERVER_VERSION;");
+  const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
 
-  response.status(200).json({ code: 200 });
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
